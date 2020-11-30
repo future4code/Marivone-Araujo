06b1fa62a97bf57c5de48219edf20388309508c7
Your Name <you@example.com>
2020-11-30 14:18:10 -0300

    criei a branch semana3-aula9, nela a pasta semana 3, dentro a pasta variaveis, dentro a pasta js-vanilla-template, outra de mesmo nome e dentro dela a pasta src, onde tem os arquivos index.css, index.html e index.js. As respostas da atividade estão em index.js

---
+++ semana3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,112 @@
+// console.log("Hello,World")
+
+// exercicio1:
+//  let a=10
+//  let b=10
+//  console.log (b)
+
+// 10
+
+//  b=5
+//  console.log (a,b)
+
+//  10,5
+
+// exercicio2:
+// let a=10
+// let b=20
+// let c
+
+// c=a
+// b=c
+// a=b
+
+// console.log(a,b,c)
+
+// 10,10,10
+
+
+// exercicio de escrita de codigo
+
+// 1.
+// a.
+// let nome
+// b.
+// let idade
+// c.
+// console.log(typeof nome)
+// undefined
+// console.log(typeof idade)
+// undefined
+// d.porque falta valor nas variáveis "nome" e "idade".
+
+// d.
+// nome = prompt("Qual é o seu nome?") 
+// console.log(nome)
+
+// idade = prompt("Qual é a sua idade?") 
+// console.log(idade)
+
+// e.
+// console.log(typeof nome)
+// string
+// console.log(typeof idade)
+// string
+// Eu achava que este typeof da idade seria number e não string 
+
+// f.
+// console.log("Olá, " + nome +". Você tem " + idade + " anos.")
+
+// 2.
+// let nome
+// let idade
+// let endereco
+// let nomeDaMae
+// let nomeDoPai
+
+// nome = prompt("Qual o seu nome?")
+// idade = prompt("Qual a sua idade?")
+// endereco = prompt("Qual o seu endereço?")
+// nomeDaMae = prompt("Qual o nome da sua mãe?")
+// nomeDoPai = prompt("Qual o nome do seu pai?")
+
+// console.log("1. Qual o seu nome? Resposta:", nome)
+// console.log("1. Qual a sua idade? Resposta:", idade)
+// console.log("1. Qual o seu endereço? Resposta:", endereco)
+// console.log("1. Qual o nome da sua mãe? Resposta:", nomeDaMae)
+// console.log("1. Qual o nome do seu pai? Resposta:", nomeDoPai)
+
+// 3.
+// a.
+// let comidas = ["acarajé", "bobó de camarão", "sushi", "lámen", "bibimbap"]
+
+// b.
+// console.log(comidas)
+// console.log("Essas são as minhas comidas preferidas:")
+// console.log (comidas[0])
+// console.log (comidas[1])
+// console.log (comidas[2])
+// console.log (comidas[3])
+// console.log (comidas[4])
+
+// c.
+// let comidaFavorita
+
+// comidaFavorita = prompt("Qual é a sua comida favorita?")
+
+// console.log("acarajé," + comidaFavorita + ",bobó de camarão, sushi, lámen, bibimbap")
+
+// 4.
+// let perguntas = ["Sua blusa é azul?", "Sua roupa é verde?", "Sua calça é preta?"]
+// a.
+// let respostas = [false, true, false]
+
+// console.log(perguntas[0], respostas[0])
+// console.log(perguntas[1], respostas[1])
+// console.log(perguntas[2], respostas[2])
+
+
+
+
+
+
