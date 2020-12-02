848db8e0aa6b726af9586e02a5b108457f86ef48
Your Name <you@example.com>
2020-12-02 15:03:00 -0300

    criei a branch semana3-aula11 a partir da master e dentro da pasta semana3 criei a pasta condicionais. no arquivo index.js, dentro da pasta js-vanilla-template, inseri as resoluções dos exercícios

---
+++ semana3/condicionais/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,83 @@
+// Exercicios de interpretacao de codigo
+// 
+// EXERCICIO 1
+// a. O código realiza uma pergunta ao usuário e, mediante a resposta, ele informa se usuário passou ou não passou no teste. 
+// b. O código testa se o número enviado pelo usuário no prompt é divisível ou não por 2 (ou seja, testa se ele é um número par).
+// c. O número par (divisível por 2).
+// d. O número impar (não divisível por 2, e que deixa resto de divisão).
+// _________________________________
+// EXERCICIO 2
+// a. O código serve para que o usuário consulte uma fruta e saiba o preço dela. 
+// b. Se o valor for Maçã, aparecerá a mensagem com o preco R$2.25 (se escrever com letra minúscula, aparece R$5.).
+// c. Aparece o preço que está no default: R$5. 
+// _________________________________
+// EXERCÍCIO3
+// const numero = Number(prompt("Digite o primeiro número."))
+
+// if(numero > 0) {
+//   console.log("Esse número passou no teste")
+// 	let mensagem = "Essa mensagem é secreta!!!"
+// }
+// console.log(mensagem)
+
+// a.  Está enviando um comando para o usuário, para que ele digite um número. O que for digitado pelo usuário será uma string transformada como variável do tipo 'Number'. O número digitado pelo usuário será guardada na "const numero". 
+
+// b. Considerando o número 10, a mensagem será "Esse número passou no teste". Se for -10, dá erro.
+
+// c. Haverá erro porque não adicionamos o "else" da sintaxe da condicional do bloco if/else. 
+// _________________________________
+
+
+
+// Exercicios de escrita de codigo
+// _________________________________
+// EXERCICIO 4
+
+    // const respostaDoUsuario = Number(prompt(`Digite sua idade`));
+   
+    // if(respostaDoUsuario > 18){
+    //     console.log(`Você pode dirigir.`)
+    // } else {
+    //     console.log(`Você não pode dirigir`)
+    // }
+
+// EXERCICIO 5
+    
+    // const turno = prompt(`Em qual turno você estuda: M, V ou N?`)
+
+    // if(turno === `M`){
+    //     console.log(`Bom dia!`)
+    // } else if (turno === `V`) {
+    //     console.log(`Boa Tarde!`)
+    // } else if (turno === `N`){
+    //     console.log(`Boa noite!`)
+    // }
+
+// EXERCICIO 6
+    // const turno = prompt(`Em qual turno você estuda: M, V ou N?`)
+
+    // switch(turno){
+    //     case `M`:
+    //         console.log(`Bom dia!`)
+    //         break
+    //     case `V`:
+    //         console.log("Boa tarde!")      
+    //         break
+    //     case `N`:
+    //         console.log("Boa noite!")
+    //         break    
+    //     }
+
+    
+// EXERCICIO 7
+
+// const genero = prompt(`Qual é o gênero do filme que você quer assistir?`)
+// const valorIngresso = prompt(`Qual é o valor do ingresso?`)  
+
+// if (genero == `fantasia` && valorIngresso < `15`){
+//     console.log(`Bom filme!`)
+// }else{
+//     console.log(`Escolha outro filme! :(`)
+// } 
+
+
