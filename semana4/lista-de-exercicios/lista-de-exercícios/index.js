e896d7528ce4ede9ac37c5eecd00c77bc492f57c
Your Name <you@example.com>
2020-12-11 16:52:37 -0300

    As três maneiras são: forEach, filter e map.Exemplo de programa incluindo as três formas foi realizado.

--- semana4/lista-de-exercicios/lista-de-exercícios/index.js
+++ semana4/lista-de-exercicios/lista-de-exercícios/index.js
@@ -1,6 +1,6 @@
 console.log("Olá mundo!")
 // Recomendamos que cada exercício esteja dentro de uma função.
-// Os exercícios de resposta aberta devem estar dentro de comentários (/**/ ou // ).
+
 
 // Exercícios de interpretação de código
 1.
@@ -11,3 +11,34 @@ console.log("Olá mundo!")
 // 3. É um código que contém três arrays, uma com valores e outras duas vazias, seguido de uma estrutura condiconal (for...of) que testa se os itens do primeiro array, array numeros, são pares ou não. Sendo pares, os números são incluídos (por meio da função/método push) na array1, mas caso não sejam pares, os números são inseridos na array2. O comando vai imprimir no console a frase: Quantidade total de números 12. Em outra linha vai imprimir 5 e em outra linha vai imprimir 7. Esses dois últimos indicam a extensão das novas array (.lenght serve para descobrir o tamanho da array).
 
 // 4. É um código que contém uma array de números chamada numeros, uma variável chamada numero1 que recebe o valor infinity e uma variável chamada numero2 que recebe o valor zero. Todos são seguidos de uma estrutura condicional (for...of) onde as variáveis numero1 e numero2 recebem novos valores conforme a condição imposta: primeiro, se o numero da array numeros for menor que o numero1, numero1 vai ser igual a este numero; segundo, se o numero da array de numeros for maior que numero2, numero2 vai ser igual a este numero. Infinity é maior que qualquer número positivo. Assim, o console vai imprimir em uma linha -10 (número negatico) e na linha abaixo 1590 (o maior número da lista). 
+
+// ------------------------------------------------
+// Exercícios de Lógica de Programação
+1.
+// // As três maneiras são: forEach, filter e map.  
+// const dadosDaFamilia=[
+//     {nome: "Marivone", idade:"38 anos", mudanças:0},
+//     {nome: "Felipe", idade:"37 anos", mudanças:0},
+//     {nome: "Ícaro", idade:"06 meses", mudanças:0},
+//     {nome: "Yuri", idade:"06 meses",mudanças:0}
+// ]
+
+// dadosDaFamilia.forEach((item, index, array) => {
+//     dadosDaFamilia.mudanças = 4
+// })
+// console.log(dadosDaFamilia)
+
+// const comAnosDeVida = dadosDaFamilia.filter((item, index,array)=>{
+//     if (item.idade === "38 anos"){
+//         return true
+//     } else{
+//         return false
+//     }
+// })
+// console.log (comAnosDeVida)
+
+// const nomesDaFamilia = dadosDaFamilia.map((item, index, array)=>{
+//     return item.nome
+// })
+
+// console.log(nomesDaFamilia)
