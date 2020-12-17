2c80591b9b0c411067c6ae537ece7e503dd67e85
Your Name <you@example.com>
2020-12-17 17:34:49 -0300

    na mesma branch semana5-aula19, na pasta semana5, na pasta insta4 desenvolvi minhas atividades de hoje

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,29 +3,46 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+
+    state = {
+      posts: [
+    {
+      nome:"paulinha",
+      foto:"https://picsum.photos/50/50",
+      fotoPost:"https://picsum.photos/200/150"
+    },
+
+    {
+      nome:"Ana Clara",
+      foto:"https://picsum.photos/51/54",
+      fotoPost:"https://picsum.photos/201/154"
+    },
+
+    {
+      nome:"Solange",
+      foto:"https://picsum.photos/52/57",
+      fotoPost:"https://picsum.photos/202/157"
+    }          
+  ]
+  }
+
+  novoPost = this.state.posts.map ((post) =>{
+    return(
+      
+      <div><p>{post.nome}</p> <img src={post.foto}></img> <img src={post.fotoPost}></img></div>  
+      
+    )
+
+  })
+
   render() {
-    return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'Ana Clara'}
-          fotoUsuario={'https://picsum.photos/51/54'}
-          fotoPost={'https://picsum.photos/201/154'}
-        />
-
-        <Post
-          nomeUsuario={'Solange'}
-          fotoUsuario={'https://picsum.photos/52/57'}
-          fotoPost={'https://picsum.photos/202/157'}
-        />
+    return(
+      <div className={`app-container`}>
+        <div>{this.novoPost}</div>
       </div>
-    );
+      )      
   }
 }
 
 export default App;
+
