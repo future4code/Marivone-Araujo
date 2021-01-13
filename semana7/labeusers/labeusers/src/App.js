10b6473bb1081af130fdacb0577c31af47effa4a
Your Name <you@example.com>
2021-01-12 21:43:24 -0300

    consegui implementar a renderização condicional, em duas páginas, mas só consegui mostrar a lista nas duas páginas, e não apenas em uma

--- semana7/labeusers/labeusers/src/App.js
+++ semana7/labeusers/labeusers/src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import axios from 'axios';
 
 
-const BotaoTopo = styled.div`
+const BotaoLista = styled.div`
 /* border: 1px solid black; */
 text-align:center;
 padding: 1px;
@@ -15,127 +15,137 @@ text-align:center;
 `
 
 export default class App extends React.Component {
-  state ={
-    usuarios:[],
+  state = {
+    usuarios: [],
     name: "",
-    email:"",
-    mostraLista: false
+    email: "",
+    defineTela: false
+
+  }
+
+  defineTela = () =>{
+    if(this.state.defineTela){
+      return(
+       <div>
+        <h3>Alunos Cadastrados</h3>
+        {/* <div>{pegarUsuarios()}</div>    */}
+        <p>
+        <button onClick ={this.voltar}>Voltar</button>
+        </p>
+        </div>
+      )
+    } else{
+      return(
+        <ContainerCentral>
+          <h3>Cadastrar Alunos</h3>
+          <p>
+            <label for="name">Nome: </label>
+            <input
+              id="name"
+              className="Name"
+              type="text"
+              value={this.state.name}
+              onChange={this.onChangeNome} />
+          </p>
+          <p>
+            <label for="email">E-mail: </label>
+            <input
+              id="email"
+              className="Email"
+              type="text"
+              value={this.state.email}
+              onChange={this.onChangeEmail} />
+          </p>
+          <p>
+            <button
+              className="salvar"
+              criarUsuario=""
+              onClick={this.criarUsuario}
+            >Salvar</button>
+          </p>
+          
+          <p>
+          <button onClick ={this.mostrarCadastrados} >Mostrar cadastrados</button>
+          </p>
+        
+        </ContainerCentral>
+      )
+    }
+  }
+  
+  mostrarCadastrados =()=>{
+    this.setState({defineTela: true})
+  }
+
+  voltar =()=>{
+    this.setState({defineTela: false})
   }
 
   componentDidMount = () => {
     this.pegarUsuarios()
   }
 
-  pegarUsuarios = () =>{
-    axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",{
-      headers:{
-        Authorization:"marivone-araujo-epps"
+  pegarUsuarios = () => {
+    axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", {
+      headers: {
+        Authorization: "marivone-araujo-epps"
       }
-    }).then((resposta) =>{
-      this.setState({usuarios:resposta.data})
-    }).catch((erro) =>{
+    }).then((resposta) => {
+      this.setState({ usuarios: resposta.data })
+    }).catch((erro) => {
       console.log(erro.message)
-      alert ("Impossível pegar usuários" + erro.message)
+      alert("Impossível pegar usuários" + erro.message)
     })
   }
 
-  onChangeNome = (e)=>{
-    this.setState({name: e.target.value})
-  }
-  
-  onChangeEmail = (e)=>{
-    this.setState({email: e.target.value})
+  onChangeNome = (e) => {
+    this.setState({ name: e.target.value })
   }
 
-  criarUsuario = () =>{
-    const body ={
+  onChangeEmail = (e) => {
+    this.setState({ email: e.target.value })
+  }
+
+  criarUsuario = () => {
+    const body = {
       name: this.state.name,
       email: this.state.email
     }
-    
-    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
-    {
-      headers:{
-        Authorization: "marivone-araujo-epps",
-      }
-    }).then(()=>{
-      alert("Cadastro realizado!")
-      this.setState({name:"", email:""})
-      this.pegarUsuarios()
-    }).catch((erro) =>{
-      console.log(body)
-      alert ("Usuário não foi cadastrado" + erro.message)
-    })
-  }
 
-  mostraLista = ()=>{
-    this.setState({mostraLista: !this.state.mostraLista})
+    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
+      {
+        headers: {
+          Authorization: "marivone-araujo-epps",
+        }
+      }).then(() => {
+        alert("Cadastro realizado!")
+        this.setState({ name: "", email: "" })
+        this.pegarUsuarios()
+      }).catch((erro) => {
+        console.log(body)
+        alert("Usuário não foi cadastrado..." + erro.message)
+      })
   }
 
   render() {
 
-    const renderedPegarUsuarios = this.state.usuarios.map((usuario) =>{
+    const renderedPegarUsuarios = this.state.usuarios.map((usuario) => {
       return <p>{usuario.name}</p>
     })
-    
-     return (
+
+    return (
 
 
-      <div>           
-    
-      <ContainerCentral>
-        <h3>Cadastro de Alunos</h3>
-        <p>
-        <label for="name">Nome: </label>
-        <input 
-        id="nome" 
-        className="InputNome" 
-        type="text" 
-        value= {this.state.name}
-        onChange={this.onChangeNome} />
-        </p>
-        
-        <p>     
-        <label for="email">E-mail: </label>
-        <input 
-        id="email" 
-        className="InputEmail" 
-        type="text" 
-        value = {this.state.email}
-        onChange = {this.onChangeEmail}/>
-        </p>
-        <p>
-        <button
-        
-        className="salvar" 
-        criarUsuario=""
-        onClick={this.criarUsuario} 
-        
-        >Salvar</button>
-        </p>
+      <div>
 
-      </ContainerCentral>
+        {this.defineTela()}
+        <div>{renderedPegarUsuarios}</div>   
 
-      <BotaoTopo>
-      <button 
-      className="BotaoPLista"
-      onClick={this.mostraLista}
-      >
-      Ir para a página da lista
-      </button>
-      </BotaoTopo>
-      
-      <div>{renderedPegarUsuarios}</div> 
 
-         
+      </div>
 
-      
-      
-      
-  </div>
-   
-  );
+    );
   }
-  
-} 
+
+}
 
---
+++ semana7/labeusers/labeusers/src/Componentes/listaCadastrados.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+// import axios from 'axios';
+
+
+export default class listaCadastrados extends React.Component {
+
+  render() {
+
+    })
+    
+     return (
+
+
+      <div>
+
+      </div>
+   
+  );
+  }
+  
+} 
+
