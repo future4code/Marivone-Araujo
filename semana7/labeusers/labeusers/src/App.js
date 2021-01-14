498c5f65ce7d78ac0dc00ffb18874cabbf9f9a26
Your Name <you@example.com>
2021-01-13 21:04:27 -0300

    fiz a componentização com a sintaxe de async e await, arrumei a limpeza dos campos dos inputs e uma estilização simples

--- semana7/labeusers/labeusers/src/App.js
+++ semana7/labeusers/labeusers/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import {ListaCadastrados} from "./Componentes/ListaCadastrados"
+import { PgCadastro } from "./Componentes/PgCadastro";
 
-
-const ContainerCentral = styled.div`
-border: 1px solid black;
+const ContainerPrincipal =  styled.div`
+/* border: 1px solid black; */
 text-align:center;
 `
-const ContainerAlunos =  styled.div`
-display: flex;
-border: 1px solid black;
+const ButtonScreenChoice =  styled.button`
+background-color: burlywood;
+margin: 50px;
+border:none;
+border-radius: 3px;
+padding: 6px;
 text-align:center;
-`
-const BotaoDeleta = styled.button`
-color: blue;
-margin: 4px;
 `
 
 
@@ -23,194 +23,28 @@ export default class App extends React.Component {
     usuarios: [],
     inputName: "",
     inputEmail: "",
-    defineTela: false
+    defineTela: true
 
   }
 
-  defineTela = () =>{
-
-      const renderedPegarUsuarios = this.state.usuarios.map((usuario) => {
-      return <p>{usuario.name}   
-             
-             <BotaoDeleta onClick= {() => {this.deletarCadastrado(usuario.id)}} >X<strong></strong></BotaoDeleta>
-             </p>})
-
-    if(this.state.defineTela){
-      return(               
-        <div>
-        <p>
-        <button onClick ={this.voltar}>Voltar</button>
-        </p>
-
-        <h3>Alunos Cadastrados</h3> 
-
-        <ContainerAlunos>       
-            
-        <div>{renderedPegarUsuarios}
-        
-        </div>          
-        </ContainerAlunos>
-        
-        </div>
-      )
-    } else{
-      return(
-        <ContainerCentral>
-          <h3>Cadastrar Alunos</h3>
-          <p>
-            <label for="name">Nome: </label>
-            <input
-              id="name"
-              className="Name"
-              type="text"
-              value={this.state.inputName}
-              onChange={this.onChangeNome} />
-          </p>
-          <p>
-            <label for="email">E-mail: </label>
-            <input
-              id="email"
-              className="Email"
-              type="text"
-              value={this.state.inputEmail}
-              onChange={this.onChangeEmail} />
-          </p>
-          <p>
-            <button
-              className="salvar"
-              criarUsuario=""
-              onClick={this.criarUsuario}
-            >Salvar</button>
-          </p>
-          
-          <p>
-          <button onClick ={this.mostrarCadastrados} >Mostrar cadastrados</button>
-          </p>
-        
-        </ContainerCentral>
-      )
-    }
-  }
-  
-  componentDidMount = () => {
-    this.pegarUsuarios()
-  }
- 
-  mostrarCadastrados =()=>{
-    this.setState({defineTela: true})
-  }
-
-  voltar =()=>{
-    this.setState({defineTela: false})
-  }
-
-  // deletarCadastrado = (id) => {
-  //   axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`, {
-  //     headers: {
-  //       Authorization: "marivone-araujo-epps"
-  //       }
-  //     }).then((resposta) => {
-  //       console.log(resposta)
-  //       this.pegarUsuarios()
-  //     }).catch((erro) =>{
-  //       console.log(erro)
-  //     })
-  // }
-  
-   deletarCadastrado = async (id) => {
-     try{
-    const resposta = await axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`, {
-      headers: {
-        Authorization: "marivone-araujo-epps"
-    }})
-      console.log(resposta)
-      this.pegarUsuarios()
-    } catch (erro) {
-        console.log(erro)
-      }
-  }
-
-  pegarUsuarios = async () => {
-    try{
-    const resposta = await axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", {
-      headers: {
-        Authorization: "marivone-araujo-epps"
-    }})
-      this.setState({ usuarios: resposta.data })
-    } catch (erro){
-      console.log(erro.message)
-      alert("Impossível pegar usuários" + erro.message)
-    }
-  }
-
-  // pegarUsuarios = () => {
-  //   axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", {
-  //     headers: {
-  //       Authorization: "marivone-araujo-epps"
-  //     }
-  //   }).then((resposta) => {
-  //     this.setState({ usuarios: resposta.data })
-  //   }).catch((erro) => {
-  //     console.log(erro.message)
-  //     alert("Impossível pegar usuários" + erro.message)
-  //   })
-  // }
-
-  onChangeNome = (e) => {
-    this.setState({ inputName: e.target.value })
-  }
-
-  onChangeEmail = (e) => {
-    this.setState({ inputEmail: e.target.value })
-  }
-
-  criarUsuario = async () => {
-    const body = {
-      name: this.state.inputName,
-      email: this.state.inputEmail
-    }
-    try {
-    const resposta = await axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
-      {
-        headers: {
-          Authorization: "marivone-araujo-epps",
-    }})
-      alert("Cadastro realizado!")
-      this.setState({ inputName: "", inputEmail: "" })
-      this.pegarUsuarios()
- 
-    } catch(erro) {
-        console.log(body)
-        alert("Usuário não foi cadastrado..." + erro.message)
-      }
-  }
-
-  // criarUsuario = () => {
-  //   const body = {
-  //     name: this.state.inputName,
-  //     email: this.state.inputEmail
-  //   }
-  //   axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
-  //     {
-  //       headers: {
-  //         Authorization: "marivone-araujo-epps",
-  //       }
-  //     }).then(() => {
-  //       alert("Cadastro realizado!")
-  //       this.setState({ inputName: "", inputEmail: "" })
-  //       this.pegarUsuarios()
-  //     }).catch((erro) => {
-  //       console.log(body)
-  //       alert("Usuário não foi cadastrado..." + erro.message)
-  //     })
-  // }
-
-
+defineTela =() =>{
+  this.setState({defineTela:!this.state.defineTela})
+} 
+   
   render() {   
     return (
       <div>
-        {this.defineTela()}             
-      </div>
+      <ContainerPrincipal>
+        
+        <h1>Labenusers</h1>        
+                
+       {this.state.defineTela ? <PgCadastro/> : <ListaCadastrados />}
+      
+      </ContainerPrincipal>
+
+       
+      <ButtonScreenChoice onClick={this.defineTela}> Página Seguinte </ButtonScreenChoice>
+</div>
 
     );
   }
--- semana7/labeusers/labeusers/src/Componentes/listaCadastrados.js
+++ semana7/labeusers/labeusers/src/Componentes/listaCadastrados.js
@@ -1,23 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
-// import axios from 'axios';
+import axios from 'axios';
+import { PgCadastro } from "./PgCadastro";
+import {axiosConfig, baseUrl, parameters} from "../parameters"
 
 
-export default class listaCadastrados extends React.Component {
+const ButtonDelete =  styled.button`
+background-color: white;
+color: red;
+margin: 10px;
+border: none;
+text-align:center;
+`
+
+export class ListaCadastrados extends React.Component {
+state = {
+    usuarios: [],
+}
+
+  componentDidMount = () => {
+    this.pegarUsuarios()
+  }
+
+  pegarUsuarios = async () => {
+    try{
+    const resposta = await axios.get( baseUrl,axiosConfig)
+
+      this.setState({ usuarios: resposta.data })
+    } catch (erro){
+      console.log(erro.message)
+      alert("Impossível pegar usuários" + erro.message)
+    }
+  }
+
+  deletarCadastrado = async (id) => {
+    try{
+    const resposta = await axios.delete(`${baseUrl}/${id}`, axiosConfig)
+      console.log(resposta)
+      this.pegarUsuarios()
+    } catch (erro) {
+        console.log(erro)
+      }
+  }
+
+
 
   render() {
+    return(
 
-    })
-    
-     return (
+    <div>      
 
+      <h3>Alunos Cadastrados</h3>
 
-      <div>
+      {this.state.usuarios.map((usuario) => {
+        return(
+          <p> {usuario.name}
 
-      </div>
-   
-  );
-  }
-  
-} 
+          <ButtonDelete onClick= {() => {this.deletarCadastrado(usuario.id)}} >X<strong></strong></ButtonDelete>
 
+          </p>
+
+        )
+      }
+      
+      )}    
+    </div>
+
+    )}} 
