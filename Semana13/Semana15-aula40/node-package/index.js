133280aacf6bb468e1b78e5da2a0c69fde2d01ca
MarivoneAraujo <74380088+MarivoneAraujo@users.noreply.github.com>
2021-03-15 17:21:14 -0300

    desafios 1 e 2 concluídos

--- Semana13/Semana15-aula40/node-package/index.js
+++ Semana13/Semana15-aula40/node-package/index.js
@@ -2,4 +2,11 @@ const nome = process.argv[2]
 const idade = Number(process.argv[3])
 const idadeFutura = Number(process.argv[4] = Number(process.argv[3]) + 7) 
 
-console.log (`Olá, ${process.argv[2]}! Você tem ${process.argv[3]}. Em sete anos você terá ${process.argv[4]}.`)
+if (nome && idade){
+   
+console.log (`\x1b[34m`,`Olá, `+nome+`! Você tem `+idade+` anos. Em sete anos você terá ${process.argv[4]}.`)
+
+}else{
+console.log(`\x1b[31m`,`Esperava 2 parâmetros só recebi um.`)
+}
+
