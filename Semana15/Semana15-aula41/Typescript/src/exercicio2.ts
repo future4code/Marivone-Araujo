142ba6eeb50ae40b769377c7e2c8a42a70920e0b
MarivoneAraujo <74380088+MarivoneAraujo@users.noreply.github.com>
2021-03-16 21:40:03 -0300

    exercícios 1 ao 4 respondidos

--- Semana15/Semana15-aula41/Typescript/src/exercicio2.ts
+++ Semana15/Semana15-aula41/Typescript/src/exercicio2.ts
@@ -1,22 +1,47 @@
 // console.log("Hello,world")
 
-function obterEstatisticas(numeros: num[]) {
+// EXERCÍCIO 2
+// a.
+// entradas: numeros, a, b
+// saídas: estatisticas
 
-    const numerosOrdenados = numeros.sort(
-        (a, b) => a - b
-    )
+// function obterEstatisticas(
+//     numeros: number[],  
+//     ) {
 
-    let soma = 0
+//     const numerosOrdenados = numeros.sort(
+//         (a: number, b:number) => a - b
+//     )
 
-    for (let num of numeros) {
-        soma += num
-    }
+//     let soma: number = 0
 
-    const estatisticas = {
-        maior: numerosOrdenados[numeros.length - 1],
-        menor: numerosOrdenados[0],
-        media: soma / numeros.length
-    }
+//     for (let num of numeros) {
+//         soma += num
+//     }
 
-    return estatisticas
-}
+//     const estatisticas = {
+//         maior: numerosOrdenados[numeros.length - 1],
+//         menor: numerosOrdenados[0],
+//         media: soma / numeros.length
+//     }
+
+//     return estatisticas
+// }
+
+// b. 1. numeros = array de números do type number; 2. a,b = variáveis do método sort do type number; 3. soma = variável to type number; 4. num = variável do for; 5. Um objeto (variável) que armazena 3 propriedades: maior, menor e media;
+
+// c. 
+
+// type estatistica = {
+//     maior: number,
+//     menor: number,
+//     media: number,
+// }
+
+// type amostraDeDados = {
+// numeros: number[],
+// obterEstatisticas: (numeros: number[]) => estatistica 
+// }
+
+// const get: estatistica = obterEstatisticas([21, 18, 65, 44, 15, 18]) 
+// console.log(get)
