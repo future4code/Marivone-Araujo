1abf03d544aed9cd3c886cf305d9c0af3f979eac
MarivoneAraujo <74380088+MarivoneAraujo@users.noreply.github.com>
2021-03-18 19:02:13 -0300

    atividades 1 a 7 concluídas

--- Semana15/Semana15-aula43/review/src/index.ts
+++ Semana15/Semana15-aula43/review/src/index.ts
@@ -9,7 +9,7 @@
 
 // // Exercício3
 // Vamos agora praticar o uso de buscas mais variáveis. Faça agora um endpoint de busca que encontre um usuário buscando por nome.
-// a. Qual é o tipo de envio de parâmetro que costuma ser utilizado por aqui? path params
+// a. Qual é o tipo de envio de parâmetro que costuma ser utilizado por aqui? path params ou query params.
 // b. Altere este endpoint para que ele devolva uma mensagem de erro caso nenhum usuário tenha sido encontrado.
 
 import express, { Request, Response } from "express";
@@ -117,26 +117,163 @@ app.get("/users/search", (req: Request, res: Response) => {
 });
 
 // Exercicio3
-app.get("/users/", (req: Request, res: Response) => {
+app.get("/users/:name", (req: Request, res: Response) => {
   let errorCode: number = 400;
   try {
-    const name: string = req.query.name as string;
-    const myUsers = users;
-    const myUser = myUsers.find((u) =>{return u.name === name})
+    const name: string = req.params.name as string;
+  
+    if (!name){
+        errorCode = 422
+        throw new Error ("Invalid value for name")
+    }
+    const myUser = users.find((u) =>{return u.name === name})
     if (!myUser){
-        errorCode = 400
+        errorCode = 404
         throw new Error ("User not found")
-    } res.status(200).send({myUser})
-
+    }     
+    res.status(200).send({myUser})
   } catch (error) {
     res.status(errorCode).send({ message: error.message });
   }
 });
 
+// Exercicio4 
+app.post ("/users", (req:Request, res: Response) =>{
+    let errorCode: number = 400
+    try {
+        // const {id, name, email, type, age} = req.body
+        const reqBody: user ={
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type,
+            age: req.body.age
+        }
+        
+        if (!reqBody.id ||
+            !reqBody.name ||
+            !reqBody.email ||
+            !reqBody.type ||
+            !reqBody.age){
+                errorCode = 422;
+                throw new Error ("Please check the fields.")
+        }
+        users.push(reqBody);
+        res.status(201).send({message: "User created successfully"})
 
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+})
 
+// Exercicio4 a e b: o usuário foi cadastrado normalmente. Como era uma inserção direta de dados, sem muitas verificações e consultas, poderia ter sido o put mesmo. 
+app.put ("/users", (req:Request, res: Response) =>{
+    let errorCode: number = 400
+    try {
+        // const {id, name, email, type, age} = req.body
+        const reqBody: user ={
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type,
+            age: req.body.age
+        }
+        
+        if (!reqBody.id ||
+            !reqBody.name ||
+            !reqBody.email ||
+            !reqBody.type ||
+            !reqBody.age){
+                errorCode = 422;
+                throw new Error ("Please check the fields.")
+        }
+        users.push(reqBody);
+        res.status(201).send({message: "User created successfully"})
 
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+})
 
+// Exercício 5
+app.put ("/users", (req:Request, res: Response) =>{
+    let errorCode: number = 400
+    try {
+        // const {id, name, email, type, age} = req.body
+        const reqBody: user ={
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type,
+            age: req.body.age
+        }
+        
+        if (!reqBody.id ||
+            !reqBody.name ||
+            !reqBody.email ||
+            !reqBody.type ||
+            !reqBody.age){
+                errorCode = 422;
+                throw new Error ("Please check the fields.")
+        }
+        users.push(reqBody);
+        // res.status(201).send({message: "User created successfully"})
+
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+})
+
+// Exercício6
+app.patch ("/users", (req:Request, res: Response) =>{
+    let errorCode: number = 400
+    try {
+        // const {id, name, email, type, age} = req.body
+        const reqBody: user ={
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type,
+            age: req.body.age
+        }
+        
+        if (!reqBody.id ||
+            !reqBody.name ||
+            !reqBody.email ||
+            !reqBody.type ||
+            !reqBody.age){
+                errorCode = 422;
+                throw new Error ("Please check the fields.")
+        }
+        users.push(reqBody);
+        res.status(201).send({message: "User created successfully"})
+
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+})
+
+// Exercício7
+app.delete("/users/:id", (req: Request, res: Response)=>{
+    try {
+        const id = Number (req.params.id)
+        if (isNaN(Number(id))){
+            throw new Error ("Invalid id type. Please send a number")
+        }
+
+        const myUsers = users;
+        const userIndex = myUsers.findIndex((ct)=>{
+            return ct.id === id
+        });
+
+        users.splice(userIndex, 1);
+        
+        res.status(200).send("Usuário excluído com sucesso")
+
+    } catch (error) {
+        res.status(400).send({status:"FAILED", message:error.message})
+    }
+})
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
