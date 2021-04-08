78ae8028f2aaed061eb21f05bce52b2bcbdd9fe8
MarivoneAraujo <74380088+MarivoneAraujo@users.noreply.github.com>
2021-04-08 09:29:44 -0300

    Atividade concluída

--- semana18/semana18-aula52/src/endpoints/createUser.ts
+++ semana18/semana18-aula52/src/endpoints/createUser.ts
@@ -10,35 +10,11 @@ export default async function createUser(
   res: Response
 ): Promise<void> {
   try {
-    const {
-      name,
-      nickname,
-      email,
-      password,
-      role,
-      cep,
-      number,
-      adicional_Info,
-    } = req.body;
-    if (
-      !name ||
-      !nickname ||
-      !email ||
-      !password ||
-      !role ||
-      !cep ||
-      !number ||
-      !adicional_Info
-    ) {
+    const {name,nickname, email, password, role, cep, number, adicional_Info} = req.body;
+    if (!name || !nickname || !email || !password || !role || !cep || !number || !adicional_Info) {
       res.statusCode = 422;
-      throw new Error(
-        "Preencha os campos 'name','nickname', 'password', 'email', 'role', 'cep', 'number', 'adicional_Info'"
-      );
-    }
-    if (role !== userRole.admin && role !== userRole.normal) {
-      throw new Error(
-        "Os valores possíveis para 'role' são 'normal' e 'admin'"
-      );
+      throw new Error("Preencha os campos 'name','nickname', 'password', 'email', 'role', 'cep', 'number', 'adicional_Info'")}
+    if (role !== userRole.admin && role !== userRole.normal) {throw new Error("Os valores possíveis para 'role' são 'normal' e 'admin'");
     }
     if (!email || email.indexOf("@") === -1 || email === "") {
       res.statusCode = 422;
@@ -58,44 +34,22 @@ export default async function createUser(
     const id: string = generateId();
     const cypherText = await hash(password);
     const newUser: user = {
-      id,
-      name,
-      nickname,
-      email,
-      password: cypherText,
-      role,
-      cep,
-      number,
-      adicional_Info,
-    };
+      id, name, nickname, email, password: cypherText, role, cep, number, adicional_Info};
     await connection("to_do_list_users").insert(newUser);
     const {
-      street,
-      neighbourhood,
-      city,
-      state,
+      street,  neighbourhood, city, state,
     }: any | null = await getAddressInfo(cep);
 
     const userAddress = {
-      cep,
-      street,
-      number,
-      adicional_Info,
-      neighbourhood,
-      city,
-      state,
-      user_id: id,
-    };
-    await connection("to_do_list_users_address").insert(userAddress);
-    
+      cep, street, number, adicional_Info, neighbourhood, city, state, user_id: id};
+    await connection("to_do_list_users_address").insert(userAddress);    
     res.status(200).send("Endereço cadastrado com sucesso.");
-
     const token: string = generateToken({ id, role });
     res.status(201).send({ token });
     // res.status(201).send("Token gerado por jwt")
   } catch (error) {
     if (res.statusCode === 200) {
-      res.status(500).send({ message: "Internal server error" });
+      res.status(500).send(error.message);
     } else {
       res.send({ message: error.message });
     }
