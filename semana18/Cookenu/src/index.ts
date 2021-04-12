960f6a38fd489a54a46b87889bc78b63eec89c55
MarivoneAraujo <74380088+MarivoneAraujo@users.noreply.github.com>
2021-04-12 18:40:41 -0300

    endpoint de deletar

--- semana18/Cookenu/src/index.ts
+++ semana18/Cookenu/src/index.ts
@@ -7,13 +7,16 @@ import loginUser from './endpoints/loginUser'
 import postRecipe from "./endpoints/postRecipe"
 import resetPassword from "./endpoints/resetPassord"
 import editRecipe from "./endpoints/editRecipe"
+import deleteRecipe from "./endpoints/deleteRecipe"
 
 app.post('/signup', createUser)
 app.post('/login', loginUser)
 app.post('/recipe', postRecipe)
 app.post ('/user/password/reset', resetPassword)
 
-app.post ('/user/edit/recipe/:id', editRecipe)
 app.get('/user/profile', getUserByProfile)
+
+app.post ('/user/edit/recipe/:id', editRecipe)
 app.get('/recipe/:id', getRecipe)
 app.get('/user/:id', getUserById)
+app.delete('/recipe/:id', deleteRecipe)
